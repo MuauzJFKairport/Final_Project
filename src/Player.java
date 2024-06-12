fd156e6d351acac8a7121d70713740618b429144
MuauzJFKairport <146112045+MuauzJFKairport@users.noreply.github.com>
2024-06-12 12:27:50 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -6,17 +6,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class Player {
-    private final double MOVE_AMT = 3; // Increase movement amount
-    private final double ROTATE_AMT = 0.022; // Reduce rotation amount
+    private final double MOVE_AMT = .25; // Reduced movement amount
+    private final double ROTATE_AMT = 0.002; // Reduced rotation amount
     private BufferedImage norm;
     private double xCoord, yCoord;
     private double angle = 0; // Rotation angle in radians
     public int score;
 
     public Player(String Img) {
-        xCoord = 770; // starting position is (50, 435), right on top of ground
+        xCoord = 770; // starting position
         yCoord = 710;
-        score = 100;
+        score = 1000;
         try {
             norm = ImageIO.read(new File(Img));
         } catch (IOException e) {
@@ -24,20 +24,29 @@ public class Player {
         }
     }
 
-    public int getxCoord() {
-        return (int) xCoord;
+    // Getter and setter methods for xCoord and yCoord
+    public double getxCoord() {
+        return xCoord;
     }
 
-    public int getyCoord() {
-        return (int) yCoord;
+    public void setxCoord(double xCoord) {
+        this.xCoord = xCoord;
+    }
+
+    public double getyCoord() {
+        return yCoord;
+    }
+
+    public void setyCoord(double yCoord) {
+        this.yCoord = yCoord;
     }
 
     public int getScore() {
         return score;
     }
 
-    public void subtractScore(){
-        score = score -1;
+    public void subtractScore() {
+        score = score - 1;
     }
 
     public void collectCoin() {
@@ -71,9 +80,24 @@ public class Player {
     }
 
     // We use a "bounding Rectangle" for detecting collision
-    public Rectangle playerRect() {
+    public Shape playerRect() {
         int imageHeight = getPlayerImage().getHeight();
         int imageWidth = getPlayerImage().getWidth();
-        return new Rectangle((int) xCoord, (int) yCoord, 63, 143);
+
+        // Reduce the width and height of the bounding rectangle
+        double reducedWidthFactor = .373;
+        double reducedHeightFactor = 0.8;
+        int reducedWidth = (int) (imageWidth * reducedWidthFactor);
+        int reducedHeight = (int) (imageHeight * reducedHeightFactor);
+
+        // Center the smaller rectangle within the player image
+        int offsetX = (imageWidth - reducedWidth) / 2;
+        int offsetY = (imageHeight - reducedHeight) / 2;
+
+        Rectangle rect = new Rectangle(offsetX, offsetY, reducedWidth, reducedHeight);
+        AffineTransform transform = new AffineTransform();
+        transform.translate(xCoord, yCoord);
+        transform.rotate(angle, imageWidth / 2.0, imageHeight / 2.0);
+        return transform.createTransformedShape(rect);
     }
 }
